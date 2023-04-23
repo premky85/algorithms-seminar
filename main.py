2b24005ae26a3c3f7366a017cd22aea78e065d8c
premky85 <leon.premky@gmail.com>
2023-04-23 18:17:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,142 @@
+import random
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+G = nx.barabasi_albert_graph(10000, 3)
+
+# z=[int(random.gammavariate(alpha=9.0,beta=2.0)) for i in range(10000)]
+# G=nx.configuration_model(z)
+
+
+
+alpha = 0.2 # infection probability
+steps = 10
+
+def avg(l):
+    return sum(l) / len(l)
+
+def calc_sir_network_model(G: nx.Graph):
+    I = set()
+    S = set()
+    R = set()
+
+    for node in G.nodes:
+        if random.random() <= alpha:
+            I.add(node)
+        else:
+            S.add(node)
+        
+    It = [len(I)]
+    St = [len(S)]
+    Rt = [0]
+    for t in range(steps):
+        _I = set()
+        _Ir = set()
+        for node in I:
+            for neighbor in G.neighbors(node):
+                if random.random() <= alpha and neighbor in S:
+                    S.remove(neighbor)
+                    _I.add(neighbor)
+
+            _Ir.add(node)
+        
+        I.update(_I)
+
+        for node in _Ir:
+            I.remove(node)
+            R.add(node)
+        
+        It.append(len(I))
+        St.append(len(S))
+        Rt.append(len(R))
+
+    lg = len(G.nodes)
+    It = [x / lg for x in It]
+    St = [x / lg for x in St]
+    Rt = [x / lg for x in Rt]
+
+    return It, St, Rt, lg
+
+    
+
+def calc_sir_network_local(G: nx.Graph, k, q):
+    node = random.choice(list(G.nodes))
+    _G = nx.ego_graph(G, node, k)
+
+    # print(len(_G.nodes))
+
+    
+    It = [[] for _ in range(steps + 1)]
+    St = [[] for _ in range(steps + 1)]
+    Rt = [[] for _ in range(steps + 1)]
+
+
+    for _ in range(q):
+        I = set()
+        S = set()
+        R = set()
+
+        for node in _G.nodes:
+            if random.random() <= alpha:
+                I.add(node)
+            else:
+                S.add(node)
+
+        It[0].append(len(I))
+        St[0].append(len(S))
+        Rt[0].append(0)
+
+        for t in range(1, steps + 1):
+            _I = set()
+            _Ir = set()
+            for node in I:
+                for neighbor in _G.neighbors(node):
+                    if random.random() <= alpha and neighbor in S:
+                        S.remove(neighbor)
+                        _I.add(neighbor)
+
+                _Ir.add(node)
+            
+            I.update(_I)
+
+            for node in _Ir:
+                I.remove(node)
+                R.add(node)
+            
+            It[t].append(len(I))
+            St[t].append(len(S))
+            Rt[t].append(len(R))
+
+    lg = len(_G.nodes)
+    It = [avg(l) / lg for l in It]
+    St = [avg(l) / lg for l in St]
+    Rt = [avg(l) / lg for l in Rt]
+
+    return It, St, Rt, lg
+
+
+                
+
+if __name__ == '__main__':
+    #G = nx.random_regular_graph(4, 10000)
+    It1, St1, Rt1, n1 = calc_sir_network_model(G)
+
+    #G = nx.random_regular_graph(4, 100)
+    It2, St2, Rt2, n2 = calc_sir_network_local(G, 4, 20)
+
+    fig, axs= plt.subplots(2)
+
+    axs[0].title.set_text(f'Full graph n: {n1}')
+    axs[0].plot(It1, label='I')
+    axs[0].plot(St1, label='S')
+    axs[0].plot(Rt1, label='R')
+
+    axs[1].title.set_text(f'Local graph n: {n2}')
+    axs[1].plot(It2, label='I')
+    axs[1].plot(St2, label='S')
+    axs[1].plot(Rt2, label='R')
+
+    axs[0].legend()
+    axs[1].legend()
+    plt.show()
